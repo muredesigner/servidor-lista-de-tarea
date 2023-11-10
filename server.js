a28ca43a8f5afeff16decee30956bb9a4e82bcad
Marycarmen Mora <muredesigner@gmail.com>
2023-11-09 23:36:00 -0400

    review 2

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const listViewRouter = require('./list-view-router');
+const listEditRouter = require('./list-edit-router');
 
-let tasks = [
-  {
-    "id": "123456",
-    "isCompleted": false,
-    "description": "Walk the dog",
-  },
-];
+// Middleware para validar los métodos HTTP de las solicitudes
+app.use((req, res, next) => {
+  const validMethods = ['GET', 'POST', 'PUT', 'DELETE'];
+  
+  if (!validMethods.includes(req.method)) {
+    return res.status(400).send('Método HTTP no válido');
+  }
 
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.use('/list-view', listViewRouter);
+app.use('/list-edit', listEditRouter);
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
 });
+
